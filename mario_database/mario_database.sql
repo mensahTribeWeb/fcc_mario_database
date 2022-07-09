d1c50a9783c54c225148b6854e5bb04d3b4d5184
mensahTribeWeb <ndm.acct85@gmailcom>
2022-07-08 22:51:07 -0500

    mario_database update

---
+++ mario_database/mario_database.sql
@@ -0,0 +1,19 @@
+SELECT *
+FROM characters
+ORDER BY character_id ASC;
+
+--UPDATE characters table
+
+UPDATE characters
+SET favorite_color = 'Yellow'
+WHERE character_id =5;
+
+UPDATE characters
+SET homeland = 'Koopa Kingdom'
+WHERE character_id =5;
+
+--SET Primary Key
+--You should set a primary key on every table and there can only be one per table.
+ALTER TABLE characters
+ADD PRIMARY KEY(name);
+
